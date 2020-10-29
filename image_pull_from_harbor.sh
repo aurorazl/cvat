9f8a51a6405c7c347533e0105207799e8d60ee53
dragon0486 <869071540@qq.com>
2020-10-29 11:50:21 +0800

    modify cvat backend and deploy

--- image_pull_from_harbor.sh
+++ image_pull_from_harbor.sh
@@ -2,7 +2,7 @@
 
 for file in docker-compose.yml docker-compose.override.yml components/analytics/docker-compose.analytics.yml components/serverless/docker-compose.serverless.yml;
 do
-    cat $file |grep image |  cut -d':' -f 2- |xargs -n 1 docker pull
+    cat $file |grep image |grep harbor |  cut -d':' -f 2- | xargs -n 1 docker pull
 done
 
 
