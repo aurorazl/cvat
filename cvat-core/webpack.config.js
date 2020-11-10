c8b7a43e3818f75b53bd6705061ba3c9b5a84f5a
Han <xjhan1995@gmail.com>
2020-11-10 15:49:01 +0800

    Update webpack pulicPath for *.worker.js

--- cvat-core/webpack.config.js
+++ cvat-core/webpack.config.js
@@ -70,7 +70,7 @@ const webConfig = {
                 use: {
                     loader: 'worker-loader',
                     options: {
-                        publicPath: '/static/engine/js/3rdparty/',
+                        publicPath: '/cvat/static/engine/js/3rdparty/',
                         name: '[name].[contenthash].js',
                     },
                 },
@@ -81,7 +81,7 @@ const webConfig = {
                 use: {
                     loader: 'worker-loader',
                     options: {
-                        publicPath: '/static/engine/js/',
+                        publicPath: '/cvat/static/engine/js/',
                         name: '[name].[contenthash].js',
                     },
                 },
