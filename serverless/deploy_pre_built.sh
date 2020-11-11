6301bf123a523ba680ef754f7f7efead573da199
dragon0486 <869071540@qq.com>
2020-11-11 16:56:02 +0800

    change for nuclio

--- serverless/deploy_pre_built.sh
+++ serverless/deploy_pre_built.sh
@@ -5,54 +5,54 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 nuctl create project cvat -n nuclio
 
 nuctl deploy faster-rcnn-inception-v2-coco -n nuclio --project-name cvat \
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.public.faster_rcnn_inception_v2_coco \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/openvino.omz.public.faster_rcnn_inception_v2_coco \
     --file "$SCRIPT_DIR/openvino/omz/public/faster_rcnn_inception_v2_coco/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common"
 
 nuctl deploy mask-rcnn-inception-resnet-v2-atrous-coco -n nuclio --project-name cvat \
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.public.mask_rcnn_inception_resnet_v2_atrous_coco \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/openvino.omz.public.mask_rcnn_inception_resnet_v2_atrous_coco \
     --file "$SCRIPT_DIR/openvino/omz/public/mask_rcnn_inception_resnet_v2_atrous_coco/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common"
 
 nuctl deploy yolo-v3-tf --project-name cvat -n nuclio \
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.public.yolo-v3-tf \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/openvino.omz.public.yolo-v3-tf \
     --file "$SCRIPT_DIR/openvino/omz/public/yolo-v3-tf/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common"
 
 nuctl deploy text-detection-0004 --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.intel.text-detection-0004 \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/openvino.omz.intel.text-detection-0004 \
     --file "$SCRIPT_DIR/openvino/omz/intel/text-detection-0004/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common"
 
 nuctl deploy intel.semantic-segmentation-adas-0001 --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.intel.semantic-segmentation-adas-0001 \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/openvino.omz.intel.semantic-segmentation-adas-0001 \
     --file "$SCRIPT_DIR/openvino/omz/intel/semantic-segmentation-adas-0001/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common"
 
 nuctl deploy person-reidentification-retail-0300 --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.intel.person-reidentification-retail-0300 \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/openvino.omz.intel.person-reidentification-retail-0300 \
     --file "$SCRIPT_DIR/openvino/omz/intel/person-reidentification-retail-300/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common"
 
-nuctl deploy openvino.dextr --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.dextr \
+nuctl deploy openvino-dextr --project-name cvat -n nuclio\
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/openvino.dextr \
     --file "$SCRIPT_DIR/openvino/dextr/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common"
 
 nuctl deploy mask-rcnn --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/tf.matterport.mask_rcnn \
-    --file "$SCRIPT_DIR/tensorflow/matterport/mask_rcnn/nuclio/function.yaml" \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/tf.matterport.mask_rcnn \
+    --file "$SCRIPT_DIR/tensorflow/matterport/mask_rcnn/nuclio/function.yaml"
 
 nuctl deploy faster-rcnn-inception-v2-coco --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/tf.faster_rcnn_inception_v2_coco \
-    --file "$SCRIPT_DIR/tensorflow/faster_rcnn_inception_v2_coco/nuclio/function.yaml" \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/tf.faster_rcnn_inception_v2_coco \
+    --file "$SCRIPT_DIR/tensorflow/faster_rcnn_inception_v2_coco/nuclio/function.yaml"
 
 nuctl deploy siammask --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/pth.foolwood.siammask \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/pth.foolwood.siammask \
     --file "$SCRIPT_DIR/pytorch/foolwood/siammask/nuclio/function.yaml" \
 
 nuctl deploy fbrs --project-name cvat -n nuclio\
-    --run-image harbor.sigsus.cn:8443/library/cvat/pth.saic-vul.fbrs \
-    --file "$SCRIPT_DIR/pytorch/saic-vul/fbrs/nuclio/function.yaml" \
+    --run-image harbor.sigsus.cn:8443/sz_gongdianju/cvat/pth.saic-vul.fbrs \
+    --file "$SCRIPT_DIR/pytorch/saic-vul/fbrs/nuclio/function.yaml"
 
 nuctl get function -n nuclio
--- serverless/stop_model.sh
+++ serverless/stop_model.sh
@@ -5,7 +5,7 @@ nuctl delete function yolo-v3-tf -n nuclio
 nuctl delete function text-detection-0004 -n nuclio
 nuctl delete function intel.semantic-segmentation-adas-0001 -n nuclio
 nuctl delete function person-reidentification-retail-0300 -n nuclio
-nuctl delete function openvino.dextr -n nuclio
+nuctl delete function openvino-dextr -n nuclio
 nuctl delete function mask-rcnn -n nuclio
 nuctl delete function faster-rcnn-inception-v2-coco -n nuclio
 nuctl delete function siammask -n nuclio
