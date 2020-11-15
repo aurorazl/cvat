5aa51c1ee78dbbb9012a104ba81cf529c20a0c14
dragon0486 <869071540@qq.com>
2020-11-15 21:16:56 +0800

    support export to platform

--- cvat/apps/engine/urls.py
+++ cvat/apps/engine/urls.py
@@ -46,7 +46,7 @@ def wrap_swagger(view):
 router = routers.DefaultRouter(trailing_slash=False)
 router.register('projects', views.ProjectViewSet)
 router.register('tasks', views.TaskViewSet)
-router.register('datas', views.DataView)
+router.register('datas', views.DataView,basename="datas")
 router.register('jobs', views.JobViewSet)
 router.register('users', views.UserViewSet)
 router.register('server', views.ServerViewSet, basename='server')
