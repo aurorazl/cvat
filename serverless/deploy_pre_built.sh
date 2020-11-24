e2f22a1276d922571d14cc86b2a6e0ff38ce412f
kgloveyou <hanjiangfeng2007@sina.com>
2020-11-24 11:06:43 +0800

    屏蔽模型

--- serverless/deploy_pre_built.sh
+++ serverless/deploy_pre_built.sh
@@ -9,17 +9,17 @@ nuctl deploy faster_rcnn_inception_v2_coco --project-name cvat \
     --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
     --platform local
 
-nuctl deploy mask_rcnn_inception_resnet_v2_atrous_coco --project-name cvat \
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.public.mask_rcnn_inception_resnet_v2_atrous_coco \
-    --file "$SCRIPT_DIR/openvino/omz/public/mask_rcnn_inception_resnet_v2_atrous_coco/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
-    --platform local
+# nuctl deploy mask_rcnn_inception_resnet_v2_atrous_coco --project-name cvat \
+#     --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.public.mask_rcnn_inception_resnet_v2_atrous_coco \
+#     --file "$SCRIPT_DIR/openvino/omz/public/mask_rcnn_inception_resnet_v2_atrous_coco/nuclio/function.yaml" \
+#     --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+#     --platform local
 
-nuctl deploy yolo-v3-tf --project-name cvat \
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.public.yolo-v3-tf \
-    --file "$SCRIPT_DIR/openvino/omz/public/yolo-v3-tf/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
-    --platform local
+# nuctl deploy yolo-v3-tf --project-name cvat \
+#     --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.public.yolo-v3-tf \
+#     --file "$SCRIPT_DIR/openvino/omz/public/yolo-v3-tf/nuclio/function.yaml" \
+#     --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+#     --platform local
 
 nuctl deploy text-detection-0004 --project-name cvat \
     --run-image harbor.sigsus.cn:8443/library/cvat/openvino.omz.intel.text-detection-0004 \
@@ -39,21 +39,21 @@ nuctl deploy person-reidentification-retail-0300 --project-name cvat \
     --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
     --platform local
 
-nuctl deploy openvino.dextr --project-name cvat \
-    --run-image harbor.sigsus.cn:8443/library/cvat/openvino.dextr \
-    --file "$SCRIPT_DIR/openvino/dextr/nuclio/function.yaml" \
-    --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
-    --platform local
+# nuctl deploy openvino.dextr --project-name cvat \
+#     --run-image harbor.sigsus.cn:8443/library/cvat/openvino.dextr \
+#     --file "$SCRIPT_DIR/openvino/dextr/nuclio/function.yaml" \
+#     --volume "$SCRIPT_DIR/openvino/common:/opt/nuclio/common" \
+#     --platform local
 
-nuctl deploy mask_rcnn --project-name cvat \
-    --run-image harbor.sigsus.cn:8443/library/cvat/tf.matterport.mask_rcnn \
-    --file "$SCRIPT_DIR/tensorflow/matterport/mask_rcnn/nuclio/function.yaml" \
-    --platform local
+# nuctl deploy mask_rcnn --project-name cvat \
+#     --run-image harbor.sigsus.cn:8443/library/cvat/tf.matterport.mask_rcnn \
+#     --file "$SCRIPT_DIR/tensorflow/matterport/mask_rcnn/nuclio/function.yaml" \
+#     --platform local
 
-nuctl deploy faster_rcnn_inception_v2_coco --project-name cvat \
-    --run-image harbor.sigsus.cn:8443/library/cvat/tf.faster_rcnn_inception_v2_coco \
-    --file "$SCRIPT_DIR/tensorflow/faster_rcnn_inception_v2_coco/nuclio/function.yaml" \
-    --platform local
+# nuctl deploy faster_rcnn_inception_v2_coco --project-name cvat \
+#     --run-image harbor.sigsus.cn:8443/library/cvat/tf.faster_rcnn_inception_v2_coco \
+#     --file "$SCRIPT_DIR/tensorflow/faster_rcnn_inception_v2_coco/nuclio/function.yaml" \
+#     --platform local
 
 nuctl deploy siammask --project-name cvat \
     --run-image harbor.sigsus.cn:8443/library/cvat/pth.foolwood.siammask \
