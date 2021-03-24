703ee016a290ec07154571342ce6b0a778256b71
Jiangfeng.Han <jiangfeng.han@apulis.com>
2021-03-24 16:25:02 +0800

    revert files

--- cvat-ui/src/cvat-core-wrapper.ts
+++ cvat-ui/src/cvat-core-wrapper.ts
@@ -6,13 +6,13 @@ import _cvat from 'cvat-core/src/api';
 
 const cvat: any = _cvat;
 
-// development
-cvat.config.backendAPI = '/annotations/api/v1';
+// // development
+// cvat.config.backendAPI = '/annotations/api/v1';
 // production
-// cvat.config.backendAPI =
-//     typeof process.env.REACT_APP_API_URL === 'undefined' ?
-//         '/annotations/api/v1' :
-//         `${process.env.REACT_APP_API_URL}/annotations/api/v1`;
+cvat.config.backendAPI =
+    typeof process.env.REACT_APP_API_URL === 'undefined' ?
+        '/annotations/api/v1' :
+        `${process.env.REACT_APP_API_URL}/annotations/api/v1`;
 
 export default function getCore(): any {
     return cvat;
