3584fb61221f064d3b26c0aa39178ae5f6cb4479
Jiangfeng.Han <jiangfeng.han@apulis.com>
2021-03-24 21:15:05 +0800

    fix: #1937，跳转地址错误

--- cvat-ui/src/locale/zh-CN/reducers/notifications-reducer.ts
+++ cvat-ui/src/locale/zh-CN/reducers/notifications-reducer.ts
@@ -1,4 +1,6 @@
 /* eslint-disable */
+import consts from 'consts';
+
 export default {
     'Could not check authorization on the server': '无法检查服务器上的授权',
     'Could not login on the server': '无法登录服务器',
@@ -17,33 +19,33 @@ export default {
     'Could not set new password on the server.': '无法在服务器上设置新密码。',
     'Could not check available auth actions': '无法检查可用的身份验证操作',
     'Could not export dataset for the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '无法导出<a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>的数据集',
+        `无法导出<a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>的数据集`,
     'Could not fetch tasks': '无法获取任务',
     'Could not upload annotation for the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '无法为<a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>上传标注',
+        `无法为<a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>上传标注`,
     'Annotations have been loaded to the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '标注已经上传到 <a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>',
+        `标注已经上传到 <a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>`,
     'Could not update <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '无法更新 <a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>',
+        `无法更新 <a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>`,
     'Could not dump annotations for the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '无法为<a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>导出标注',
+        `无法为<a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>导出标注`,
     'Could not delete the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '无法删除 <a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>',
+        `无法删除 <a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>`,
     'Could not create the task': '无法创建任务',
     'Could not get formats from the server': '无法从服务器获取格式',
     'Could not get users from the server': '无法从服务器获取用户',
     'Could not get info about the server': '无法获取有关服务器的信息',
     'Could not load share data from the server': '无法从服务器加载共享数据',
     'Automatic annotation finished for the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '<a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>自动标注已经完成',
+        `<a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>自动标注已经完成`,
     'Could not fetch models meta information': '无法获取模型元数据信息',
     'Fetching inference status for the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '获取 <a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>推理状态',
+        `获取 <a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>推理状态`,
     'Could not get models from the server': '无法从服务器获取模型',
     'Could not infer model for the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '无法为<a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>推理模型',
+        `无法为<a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>推理模型`,
     'Could not cancel model inference for the <a href="/tasks/${taskID}" target="_blank">task ${taskID}</a>':
-        '无法取消<a href="/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>的模型推理',
+        `无法取消<a href="${consts.BASENAME}/tasks/{{taskID}}" target="_blank">任务 {{taskID}}</a>的模型推理`,
     'Error during fetching a job': '获取作业出错',
     'Could not receive frame ${action.payload.number}': '无法获取帧 ${action.payload.number}',
     'Could not save annotations': '无法保存标注',
@@ -57,7 +59,7 @@ export default {
     'Could not collect annotations statistics': '无法收集标注统计信息',
     'Could not save the job on the server': '无法保存作业到服务器',
     'Could not upload annotations for the <a href="/tasks/${taskID}/jobs/${jobID}" target="_blank">job ${taskID}</a>':
-        '无法为<a href="/tasks/{{taskID}}/jobs/{{jobID}}" target="_blank">作业 {{taskID}}</a>上传标注',
+        `无法为<a href="${consts.BASENAME}/tasks/{{taskID}}/jobs/{{jobID}}" target="_blank">作业 {{taskID}}</a>上传标注`,
     'Could not remove annotations': '无法移除标注',
     'Could not fetch annotations': '无法获取标注',
     'Could not redo': '无法恢复',
